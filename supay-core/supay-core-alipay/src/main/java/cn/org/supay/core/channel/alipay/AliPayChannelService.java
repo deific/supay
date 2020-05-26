ba809d6d358cfbab9822ce64ce0145ae56976256
deific <deific@126.com>
2020-05-26 14:09:58 +0800

    feat:新增支付宝支付接口实现

--- supay-core/supay-core-alipay/src/main/java/cn/org/supay/core/channel/alipay/AliPayChannelService.java
+++ supay-core/supay-core-alipay/src/main/java/cn/org/supay/core/channel/alipay/AliPayChannelService.java
@@ -5,9 +5,17 @@
 package cn.org.supay.core.channel.alipay;
 
 import cn.org.supay.core.channel.BasePayChannelService;
+import cn.org.supay.core.config.SupayChannelConfig;
 import cn.org.supay.core.context.SupayContext;
 import cn.org.supay.core.data.Request;
 import cn.org.supay.core.data.Response;
+import com.alipay.easysdk.factory.Factory;
+import com.alipay.easysdk.kernel.BaseClient;
+import com.alipay.easysdk.payment.common.models.AlipayTradeCreateResponse;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * <b>Application name：</b> AliPayChannelService.java <br>
@@ -18,7 +26,11 @@ import cn.org.supay.core.data.Response;
  * <b>@author：</b> <a href="mailto:deific@126.com"> deific </a> <br>
  * <b>@version：</b>V1.0.0 <br>
  */
+@Slf4j
 public class AliPayChannelService implements BasePayChannelService {
+    /** 阿里支付客户端 */
+    private Map<String, Factory> factoryMap = new HashMap<>();
+
     @Override
     public String getPayServiceName() {
         return "aliPayChannelService";
@@ -26,6 +38,16 @@ public class AliPayChannelService implements BasePayChannelService {
 
     @Override
     public SupayContext<? extends Request, ? extends Response> pay(SupayContext<? extends Request, ? extends Response> ctx) {
+        try {
+            // 快捷支付
+            AlipayTradeCreateResponse response = Factory.Payment.Common().create("", "null", "null", "null");
+            if ("10000".equals(response.code)) {
+                log.debug("支付成功");
+            }
+        } catch (Exception e) {
+
+        }
+
         return null;
     }
 
@@ -47,5 +69,32 @@ public class AliPayChannelService implements BasePayChannelService {
     @Override
     public SupayContext<? extends Request, ? extends Response> sendRedPackage(SupayContext<? extends Request, ? extends Response> ctx) {
         return null;
+    }
+
+    /**
+     * 获取阿里支付客户端
+     * @param channelConfig
+     * @return
+     */
+    private void init(SupayChannelConfig channelConfig) {
+        BaseClient.Config config = new BaseClient.Config();
+        config.protocol = "https";
+        config.gatewayHost = "openapi.alipay.com";
+        config.signType = "RSA2";
+
+        // 请更换为您的AppId
+        config.appId = channelConfig.getAppId();
+        // 请更换为您的应用公钥证书文件路径
+        config.merchantCertPath = "/home/foo/appCertPublicKey_2019051064521003.crt";
+        // 请更换为您的支付宝公钥证书文件路径
+        config.alipayCertPath = "/home/foo/alipayCertPublicKey_RSA2.crt";
+        // 请更换为您的支付宝根证书文件路径
+        config.alipayRootCertPath = "/home/foo/alipayRootCert.crt";
+        // 请更换为您的PKCS8格式的应用私钥
+        config.merchantPrivateKey = "MIIEvQIBADANB ... ...";
+
+        // 如果采用非证书模式，则无需赋值上面的三个证书路径，改为赋值如下的支付宝公钥字符串即可
+        // config.alipayPublicKey = "MIIBIjANBg...";
+        Factory.setOptions(config);
     }
 }
---
+++ supay-core/supay-core-alipay/src/main/java/cn/org/supay/core/channel/alipay/data/AliPayBaseRequest.java
@@ -0,0 +1,21 @@
+/*******************************************************************************
+ * @(#)AliPayBaseRequest.java 2017年04月18日 17:33
+ * Copyright 2020 supay.org.cn. All rights reserved.
+ *******************************************************************************/
+package cn.org.supay.core.channel.alipay.data;
+
+import cn.org.supay.core.data.Request;
+
+/**
+ * <b>Application name：</b> AliPayBaseRequest.java <br>
+ * <b>Application describing： </b> <br>
+ * <b>Copyright：</b> Copyright &copy; 2020 supay.org.cn/ 版权所有。<br>
+ * <b>Company：</b> supay.org.cn/ <br>
+ * <b>@Date：</b> 2020年05月16日 09:37 <br>
+ * <b>@author：</b> <a href="mailto:deific@126.com"> deific </a> <br>
+ * <b>@version：</b>V1.0.0 <br>
+ */
+public class AliPayBaseRequest extends Request {
+
+
+}
