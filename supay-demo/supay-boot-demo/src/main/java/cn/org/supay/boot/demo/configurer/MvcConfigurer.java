00c8ba183265c62d3e8a1f79565788772bf4877b
deific <deific@126.com>
2020-06-05 00:28:14 +0800

    优化渠道服务注册逻辑：自动注册

---
+++ supay-demo/supay-boot-demo/src/main/java/cn/org/supay/boot/demo/configurer/MvcConfigurer.java
@@ -0,0 +1,35 @@
+/*******************************************************************************
+ * @(#)MvcConfigurer.java 2020年05月30日 09:07
+ * Copyright 2020 http://supay.org.cn All rights reserved.
+ *******************************************************************************/
+package cn.org.supay.boot.demo.configurer;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * <b>Application name：</b> MvcConfigurer.java <br>
+ * <b>Application describing： </b> <br>
+ * <b>Copyright：</b> Copyright &copy; 2020 supay.org.cn/ 版权所有。<br>
+ * <b>Company：</b> supay.org.cn/ <br>
+ * <b>@Date：</b> 2020年05月30日 09:07 <br>
+ * <b>@author：</b> <a href="mailto:deific@126.com"> deific </a> <br>
+ * <b>@version：</b>V1.0.0 <br>
+ */
+@Configuration
+public class MvcConfigurer implements WebMvcConfigurer {
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/error").setViewName("error.html");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    }
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.setUseSuffixPatternMatch(false);
+    }
+}
--- supay-demo/supay-boot-demo/src/main/java/cn/org/supay/boot/demo/controller/AlipayDemoController.java
+++ supay-demo/supay-boot-demo/src/main/java/cn/org/supay/boot/demo/controller/AlipayDemoController.java
@@ -22,6 +22,7 @@ import cn.org.supay.core.pay.SupayCore;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.math.BigDecimal;
 import java.util.UUID;
