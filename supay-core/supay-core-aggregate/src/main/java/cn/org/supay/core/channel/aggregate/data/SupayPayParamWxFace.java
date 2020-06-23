e20d3a7d71db694bf81212e23db522672cf31d69
deific <deific@126.com>
2020-06-23 11:29:55 +0800

    完善聚合支付支付参数定义

---
+++ supay-core/supay-core-aggregate/src/main/java/cn/org/supay/core/channel/aggregate/data/SupayPayParamWxFace.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * @(#)SupayPayParamWxFace.java 2020年05月15日 22:44
+ * Copyright 2020 http://supay.org.cn All rights reserved.
+ *******************************************************************************/
+package cn.org.supay.core.channel.aggregate.data;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+/**
+ * <b>Application name：</b> SupayPayParamWxFace.java <br>
+ * <b>Application describing： </b> <br>
+ * <b>Copyright：</b> Copyright &copy; 2020 supay.org.cn/ 版权所有。<br>
+ * <b>Company：</b> supay.org.cn/ <br>
+ * <b>@Date：</b> 2020年05月15日 22:44 <br>
+ * <b>@author：</b> <a href="mailto:deific@126.com"> deific </a> <br>
+ * <b>@version：</b>V1.0.0 <br>
+ */
+@Data
+@SuperBuilder
+@NoArgsConstructor
+public class SupayPayParamWxFace {
+
+}
