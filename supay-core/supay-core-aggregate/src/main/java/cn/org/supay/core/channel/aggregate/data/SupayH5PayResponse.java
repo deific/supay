558634e0800107a24550ff4b0a6d5142db9d455d
deific <deific@126.com>
2020-07-02 13:56:25 +0800

    新增H5支付demo

---
+++ supay-core/supay-core-aggregate/src/main/java/cn/org/supay/core/channel/aggregate/data/SupayH5PayResponse.java
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * @(#)SupayPagePayResponse.java 2020年05月29日 12:26
+ * Copyright 2020 http://supay.org.cn All rights reserved.
+ *******************************************************************************/
+package cn.org.supay.core.channel.aggregate.data;
+
+import lombok.Data;
+import lombok.experimental.SuperBuilder;
+
+/**
+ * <b>Application name：</b> SupayPagePayResponse.java <br>
+ * <b>Application describing： </b> <br>
+ * <b>Copyright：</b> Copyright &copy; 2020 supay.org.cn/ 版权所有。<br>
+ * <b>Company：</b> supay.org.cn/ <br>
+ * <b>@Date：</b> 2020年05月29日 12:26 <br>
+ * <b>@author：</b> <a href="mailto:deific@126.com"> deific </a> <br>
+ * <b>@version：</b>V1.0.0 <br>
+ */
+@Data
+@SuperBuilder
+public class SupayH5PayResponse extends SupayPayResponse {
+
+}
